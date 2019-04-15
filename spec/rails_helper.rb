a10838277ba2dadce4d8dbd4a381257a717a2516
Olivier Cardinaux <ocardinaux@yahoo.fr>
2019-04-15 22:13:12 +0200

    --amend

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -5,11 +5,14 @@ require 'spec_helper'
 ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../config/environment', __dir__)
 
+abort("The Rails environment is running in production mode!") if Rails.env.production?
+
 Dir["./spec/support/**/*.rb"].sort.each { |f| require f }
 
-abort("The Rails environment is running in production mode!") if Rails.env.production?
 require 'rspec/rails'
 require 'faker'
+require 'simplecov'
+SimpleCov.start
 
 begin
   ActiveRecord::Migration.maintain_test_schema!
