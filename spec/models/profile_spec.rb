5559a726460b0e78389c8f1080b19f1e77f8e878
TheMindset <mulezig2014@outlook.fr>
2019-04-18 13:53:51 +0200

    add some rspec for User and Profile

--- spec/models/profile_spec.rb
+++ spec/models/profile_spec.rb
@@ -24,5 +24,16 @@
 require 'rails_helper'
 
 RSpec.describe Profile, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'Model instantiation' do
+    subject(:new_profile) { described_class.new }
+
+    describe 'Database' do
+      it { is_expected.to have_db_column(:id).of_type(:integer) }
+      it { is_expected.to have_db_column(:address).of_type(:string).with_options(null: true) }
+      it { is_expected.to have_db_column(:description).of_type(:text).with_options(null: true) }
+      it { is_expected.to have_db_column(:phone_number).of_type(:integer).with_options(null: true) }
+      it { is_expected.to have_db_column(:created_at).of_type(:datetime).with_options(null: false) }
+      it { is_expected.to have_db_column(:updated_at).of_type(:datetime).with_options(null: false) }
+    end
+  end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -24,5 +24,17 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'Model instantiation' do
+    subject(:new_user) { described_class.new }
+
+    describe 'Database' do
+      it { is_expected.to have_db_column(:id).of_type(:integer) }
+      it { is_expected.to have_db_column(:first_name).of_type(:string).with_options(null: true) }
+      it { is_expected.to have_db_column(:last_name).of_type(:string).with_options(null: true) }
+      it { is_expected.to have_db_column(:email).of_type(:string).with_options(null: false) }
+      it { is_expected.to have_db_column(:encrypted_password).of_type(:string).with_options(null: false) }
+      it { is_expected.to have_db_column(:created_at).of_type(:datetime).with_options(null: false) }
+      it { is_expected.to have_db_column(:updated_at).of_type(:datetime).with_options(null: false) }
+    end
+  end
 end
