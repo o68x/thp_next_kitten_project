034ab4c8e45de902a8e45a07236cd8bd014e813f
belette06 <cyril.avronsart@gmail.com>
2019-04-24 03:28:18 +0200

    wiev user ok v1

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  #root 'static_pages#home'
+  # root 'static_pages#home'
   devise_for :users
   resources :cats
-  #root 'cats#index'
+  # root 'cats#index'
   root 'cats#index'
 end
