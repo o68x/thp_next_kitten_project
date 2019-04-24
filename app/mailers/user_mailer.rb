41df29c59074d7a993dd40f60b7ca1d644d9721a
vincelep <vincent.leparoux@gmail.com>
2019-04-24 17:05:50 +0200

    config User_mailer

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,2 +1,12 @@
 class UserMailer < ApplicationMailer
+    default from: 'no-reply@monsite.fr'
+ 
+    def welcome_email(user)
+      @user = user 
+  
+      @url  = 'http://monsite.fr/login' 
+  
+      mail(to: @user.email, subject: 'Bienvenue chez nous, !') 
+    end
+  end
 end
