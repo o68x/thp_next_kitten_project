fd17c0c5abc457cf358163e74caeba38d0334781
Olivier Cardinaux <ocardinaux@gmail.com>
2019-04-25 16:40:01 +0200

    temp disable send mail
    debug heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -40,7 +40,7 @@ Rails.application.configure do
 
   # Store uploaded files on the local file system (see config/storage.yml for options)
   config.active_storage.service = :local
-
+  config.action_mailer.perform_deliveries = false
   # Mount Action Cable outside main process or domain
   # config.action_cable.mount_path = nil
   # config.action_cable.url = 'wss://example.com/cable'
