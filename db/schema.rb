4f7547325f9f78480a794cfb15c25f554dbf8d89
NIcoZF <nico-7@hotmail.fr>
2019-04-30 16:49:16 +0200

    Add cat new form

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
-ActiveRecord::Schema.define(version: 2019_04_29_124031) do
-=======
 ActiveRecord::Schema.define(version: 2019_04_30_110754) do
->>>>>>> master
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
