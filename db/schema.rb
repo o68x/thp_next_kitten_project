4df7b397fe5a9f21a3427a201fded809616f9e1f
NIcoZF <nico-7@hotmail.fr>
2019-05-07 16:31:11 +0200

    refacto admin cat index tab

--- db/schema.rb
+++ db/schema.rb
@@ -88,8 +88,6 @@ ActiveRecord::Schema.define(version: 2019_05_05_183053) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "first_name"
-    t.string "last_name"
     t.boolean "is_admin", default: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
