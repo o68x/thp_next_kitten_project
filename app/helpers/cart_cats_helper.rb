0687f6475c0e5118b67ae82b1e8df9cb5f8d0418
Olivier Cardinaux <ocardinaux@yahoo.fr>
2019-05-08 11:11:54 +0200

    add details information to seller orders table

--- app/helpers/cart_cats_helper.rb
+++ app/helpers/cart_cats_helper.rb
@@ -5,11 +5,12 @@ module CartCatsHelper
     cart_cats = CartCat.where(cat: cat_id)
     cats_data = []
     cart_cats.each do |cart_cat|
+      cart = Cart.find(cart_cat.cart_id)
       cat_data_for_seller = {}
-      cat_data_for_seller[:user] = Cart.find(cart_cat.cart_id).user.profile.first_name
-      cat_data_for_seller[:date] = Cart.find(cart_cat.cart_id).order_placed.strftime("%F")
+      cat_data_for_seller[:user] = cart.user.profile.first_name
+      cat_data_for_seller[:date] = cart.order_placed.strftime("%F")
       cat_data_for_seller[:quantity] = cart_cat.quantity
-      cat_data_for_seller[:total_price] = cart_cat.quantity
+      cat_data_for_seller[:total_price] = cart_cat.total_cat_price
       cats_data << cat_data_for_seller
     end
     p cats_data
