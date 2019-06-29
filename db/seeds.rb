51af7f5f50735ace91838cbc60b7855541aec7b5
TheMindset <mulezig2014@outlook.fr>
2019-06-29 16:51:25 +0200

    fix image bug

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,6 +8,7 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 require 'open-uri'
+require 'faker'
 ActionMailer::Base.perform_deliveries = false
 
 sleep(1)
